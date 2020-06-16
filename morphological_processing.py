fadab9f596a82b0a92ea223877246729ba5bb78f
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2020-06-17 00:09:25 +0700

    Modify things a little bit

--- morphological_processing.py
+++ morphological_processing.py
@@ -3,10 +3,18 @@ import cv2
 import matplotlib.pyplot as plt
 
 class morphological_processing:
-    def __init__(self):
-        pass
+    def __init__(self, baw_image):
+        """
+        Create instance to process an image
 
-    def morph(self, baw_image, str_element, type = None):
+        baw_image (2D numpy array): black and white image.
+        Better if image is binary. Should perform thresholding on image first.
+        The background should be white and the object is in shades of black.
+
+        """
+        self.image = baw_image
+
+    def morph(self, str_element, type = None):
         """
         Morphological processing of an image using a predefined structure element
 
@@ -15,39 +23,35 @@ class morphological_processing:
             Can be of any size (m, n) but odd numbers are recommended.
             Pixels can be zero or one, depending on the desired shape.
 
-            baw_image (2D numpy array): black and white image.
-            The background should be white and the object is in shades of black.
-
-            type: "dilation", "erosion", "open", "close", "border", "tophat", "bottomhat"
+            type: "dilation", "erosion", "open", "close", "gradient", "tophat", "bottomhat"
 
         Returns:
             new_image (2D numpy array)
 
         """
 
-        self.image = baw_image
         self.str_element = str_element
 
         if type == "dilation" or type == "erosion":
-            return self.process(self.image, self.type)
+            return self.__process__(self.image, type)
 
         elif type == "open":
-            return self.process(self.process(self.image, type="erosion"), type="dilation")
+            return self.__process__(self.__process__(self.image, type="erosion"), type="dilation")
 
         elif type == "close":
-            return self.process(self.process(self.image, type="dilation"), type="erosion")
+            return self.__process__(self.__process__(self.image, type="dilation"), type="erosion")
 
-        elif type == "border":
-            return self.process(self.image, type="dilation") - self.process(self.image, type="erosion")
+        elif type == "gradiant":
+            return self.__process__(self.image, type="dilation") - self.__process__(self.image, type="erosion")
 
         elif type == "tophat":
-            return self.image - self.morph(self.image, self.str_element, type="open")
+            return self.image - self.morph(self.str_element, type="open")
 
         elif type == "bottomhat":
-            return self.morph(self.image, self.str_element, type="close") - self.image
+            return self.morph(self.str_element, type="close") - self.image
 
 
-    def process(self, baw_image, type):
+    def __process__(self, baw_image, type):
 
         baw_image = baw_image.astype(float)
         col, row = baw_image.shape
@@ -103,8 +107,8 @@ if __name__ == "__main__":
     plt.xticks([]), plt.yticks([])
     # plt.show()
 
-    new_image = morphological_processing()
-    new_image = new_image.morph(baw_image, str_element, type="bottomhat")
+    new_image = morphological_processing(baw_image)
+    new_image = new_image.morph(str_element, type="erosion")
 
     plt.imshow(new_image, cmap='gray', interpolation='bicubic')
     plt.xticks([]), plt.yticks([])
