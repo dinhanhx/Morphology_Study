584cb5bd106422571b94b45a162fa282dc33e02f
dinhanhx <dinhanhx@gmail.com>
2020-06-16 16:29:34 +0700

    Fix 2 problems
    Swapped 'dil' and 'ero'
    
    Added nbh_row cases

--- very-simple-morphological.py
+++ very-simple-morphological.py
@@ -2,15 +2,16 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-n = 17
+n = 3
 strel = np.ones((n, n))
 # structuring element, can change to different shapes like cross or something
 # does not have to be square
 
-baw_img = cv2.imread(r'46913809661_f7f7c35fe3_o.jpg', cv2.IMREAD_GRAYSCALE)
+baw_img = cv2.imread(r'dataset/hand/1.png', cv2.IMREAD_GRAYSCALE)
 # any image
 
 def morph (baw_image, strel, type = None):
+
     baw_image = baw_image.astype(float)
 
     col, row = baw_image.shape
@@ -20,7 +21,9 @@ def morph (baw_image, strel, type = None):
     nbh_row = nbh_row_full // 2
     nbh_col = nbh_col_full // 2
 
-    baw_image = np.insert(baw_image, np.zeros(nbh_row), np.nan, axis = 0)
+    if nbh_row == 1:
+    	baw_image = np.insert(baw_image, 0, np.nan, axis = 0) 
+    else: baw_image = np.insert(baw_image, np.zeros(nbh_row), np.nan, axis = 0)
     if nbh_col == 1: baw_image = np.insert(baw_image, 0, np.nan, axis = 1)
     else: baw_image = np.insert(baw_image, np.zeros(nbh_col), np.nan, axis = 1)
     baw_image = np.insert(baw_image, np.full(nbh_row, len(baw_image)), np.nan, axis = 0)
@@ -29,13 +32,11 @@ def morph (baw_image, strel, type = None):
     for i in range(col):
         for j in range(row):
             sample = baw_image[i:i+nbh_col_full, j:j+nbh_row_full]
-            if type == "dil": new_image[i, j] = np.nanmin(sample)
-            if type == "ero": new_image[i, j] = np.nanmax(sample)
+            if type == "ero": new_image[i, j] = np.nanmin(sample)
+            if type == "dil": new_image[i, j] = np.nanmax(sample)
     return new_image.astype(int)
 
-new_img = morph(baw_img, strel, type="ero")
-plt.imshow(new_img, cmap='gray', interpolation='bicubic')
+new_img = morph(baw_img, strel, type="dil")
+plt.imshow(new_img, cmap='gray')
 plt.xticks([]), plt.yticks([])
 plt.show()
-
-
