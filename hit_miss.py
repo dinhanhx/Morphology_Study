df08eedc7d696a2966abb812569ff9cd94190fdc
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-21 20:51:39 +0700

    Plan functions to implement

---
+++ hit_miss.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020  Ngô Ngọc Đức Huy
+from cv2 import morphologyEx, threshold
+import matplotlib.pyplot as plt
+
+
+def to_binary(image):
+    """Convert a grayscale image to a binary image."""
+    pass
+
+
+def hit_miss(image):
+    """Apply hit-or-miss transform on a binary image."""
+    pass
+
+
+if __name__ == "__main__":
+    pass
