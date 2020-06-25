85f778b1db6723ec6b5f2722ae1ea9efc3e6ac85
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-25 20:23:14 +0700

    Try hit-or-miss with various kernels, resolving #7

--- hit_miss.py
+++ hit_miss.py
@@ -2,32 +2,51 @@
 from cv2 import (
     imread, blur,
     cvtColor, COLOR_BGR2GRAY,
-    threshold, THRESH_BINARY,
+    threshold, THRESH_BINARY_INV,
     morphologyEx, MORPH_HITMISS
 )
-from numpy import ones, uint8
+from numpy import ones, uint8, array
 import matplotlib.pyplot as plt
+
+kernel_list = [
+    ones((3, 3), uint8),
+    ones((5, 5), uint8),
+    array([[0, 0, 0], [0, 1, 0], [0, 0, 0]], uint8),
+    array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], uint8),
+    array([[0, 0, 0], [1, 1, 1], [0, 0, 0]], uint8),
+    array([[0, 1, 0], [0, 1, 0], [0, 1, 0]], uint8),
+    array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], uint8),
+    array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], uint8),
+    array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], uint8),
+    array([[0, 1, 0], [1, -1, 1], [0, 1, 0]], uint8),
+    array([[0, -1, -1], [1, 1, -1], [0, 1, 0]], uint8),
+    array([[-1, -1, 0], [-1, 1, 0], [-1, -1, 0]], uint8),
+]
 
 
 def to_binary(image, thr):
     """Convert a grayscale image to a binary image."""
     image = cvtColor(image, COLOR_BGR2GRAY)
     image = blur(image, (3, 3))
-    return threshold(image, thr, 255, THRESH_BINARY)[1]
+    return threshold(image, thr, 255, THRESH_BINARY_INV)[1]
 
 
-def hit_miss(image):
+def hit_miss(image, kernel):
     """Apply hit-or-miss transform on a binary image."""
-    image = morphologyEx(image, MORPH_HITMISS, ones((5, 5), uint8))
+    image = morphologyEx(image, MORPH_HITMISS, kernel)
     return image
 
 
 if __name__ == "__main__":
     img = imread('dataset/captcha/1.jpg')
-    res = to_binary(img, 100)
-    plt.subplot(211)
-    plt.imshow(res, cmap='gray')
-    plt.subplot(212)
-    res = hit_miss(res)
-    plt.imshow(res, cmap='gray')
+    img = to_binary(img, 100)
+    plt.subplot(5, 3, 2)
+    plt.imshow(img, cmap='gray')
+    i = 4
+
+    for kernel in kernel_list:
+        res = hit_miss(img, kernel)
+        plt.subplot(5, 3, i)
+        i += 1
+        plt.imshow(res, cmap='gray')
     plt.show()
