74f8d54a9eab277afc0be0cc4342915eb92030da
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-25 11:16:12 +0700

    Write code to apply hit-or-miss transformation

--- hit_miss.py
+++ hit_miss.py
@@ -1,17 +1,33 @@
 # Copyright (C) 2020  Ngô Ngọc Đức Huy
-from cv2 import morphologyEx, threshold
+from cv2 import (
+    imread, blur,
+    cvtColor, COLOR_BGR2GRAY,
+    threshold, THRESH_BINARY,
+    morphologyEx, MORPH_HITMISS
+)
+from numpy import ones, uint8
 import matplotlib.pyplot as plt
 
 
-def to_binary(image):
+def to_binary(image, thr):
     """Convert a grayscale image to a binary image."""
-    pass
+    image = cvtColor(image, COLOR_BGR2GRAY)
+    image = blur(image, (3, 3))
+    return threshold(image, thr, 255, THRESH_BINARY)[1]
 
 
 def hit_miss(image):
     """Apply hit-or-miss transform on a binary image."""
-    pass
+    image = morphologyEx(image, MORPH_HITMISS, ones((5, 5), uint8))
+    return image
 
 
 if __name__ == "__main__":
-    pass
+    img = imread('dataset/captcha/1.jpg')
+    res = to_binary(img, 100)
+    plt.subplot(211)
+    plt.imshow(res, cmap='gray')
+    plt.subplot(212)
+    res = hit_miss(res)
+    plt.imshow(res, cmap='gray')
+    plt.show()
