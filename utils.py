3c0be2c413f84fa0cc543fd3940ddbcaaba7e08c
Dinh Anh <dinhanhx@gmail.com>
2020-07-04 19:10:12 +0700

    Fix typo and identation

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
-import urllib
+import urllib.request
 import cv2
-import numpy
+import numpy as np
 
 def url_2_img(url):
     """
     Get an image from url as img object in grayscale
     """
-    resp = urllib.urlopen(url)
-	image = np.asarray(bytearray(resp.read()), dtype="uint8")
-	image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
     return image
