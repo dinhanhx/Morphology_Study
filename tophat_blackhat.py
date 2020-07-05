f206ac0835d9518fa104d5425b2e83f6661b299f
Dinh Anh <dinhanhx@gmail.com>
2020-07-05 19:05:34 +0700

    Add imshow()

--- tophat_blackhat.py
+++ tophat_blackhat.py
@@ -20,5 +20,22 @@ img_blackhat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
 # cv2.imshow('Blackhat', img_blackhat)
 # cv2.waitKey(0)
 
-cv2.imwrite('output/cancer-vs-normal-call-tophat.jpg', img_tophat)
-cv2.imwrite('output/cancer-vs-normal-call-blackhat.jpg', img_blackhat)
+# cv2.imwrite('output/cancer-vs-normal-call-tophat.jpg', img_tophat)
+# cv2.imwrite('output/cancer-vs-normal-call-blackhat.jpg', img_blackhat)
+
+import matplotlib.pyplot as plt
+plt.subplot(311)
+plt.imshow(img, cmap = 'gray')
+plt.title('The Original')
+
+plt.subplot(312)
+plt.imshow(img_tophat, cmap = 'binary_r')
+plt.title('White top-hat transform')
+
+plt.subplot(313)
+plt.imshow(img_blackhat, cmap = 'binary_r')
+plt.title('Bottom-hat transform')
+
+plt.tight_layout()
+# plt.show()
+plt.savefig('output/cancer.png')
